f0a8637ef88ce45a4c0a670b1f81fd118977b79d
PriyeshPandey2000 <priyeshpandey2000@gmail.com>
2023-11-04 11:16:14 +0530

    first commit

---
+++ src/components/Balance.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Balance({transactions}) {
+    console.log(transactions)
+   const amount=transactions.map((transaction)=> Number(transaction.amount))
+   const total =amount.reduce((acc,item)=>(acc+item),0)
+  return (
+    <div>
+        <h4>Balance</h4>
+        <h1>$ {total}</h1>
+        </div>
+  )
+}
